dc6eceda362620c6d984d1f09e51ab667bd41249
methodcase <methodcase@gmail.com>
2015-02-18 12:29:07 +0800

    react todo

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
-var Box = React.createClass({
+var TodoList = React.createClass({
+  render: function() {
+    var createItem = function(itemText) {
+      return <li>{itemText}</li>;
+    };
+    return <ul>{this.props.items.map(createItem)}</ul>;
+  }
+});
+var TodoApp = React.createClass({
   getInitialState: function() {
-    return {value: 'Hello!'};
+    return {items: [], text: ''};
   },
-  handleChange: function(event) {
-    this.setState({value: event.target.value});
+  onChange: function(e) {
+    this.setState({text: e.target.value});
+  },
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var nextItems = this.state.items.concat([this.state.text]);
+    var nextText = '';
+    this.setState({items: nextItems, text: nextText});
   },
   render: function() {
-    var value = this.state.value;
-    return (<div> <input type="text" value={value} onChange={this.handleChange} /> <span> {this.state.value} </span> </div>);
+    return (
+      <div>
+        <h3>TODO</h3>
+        <TodoList items={this.state.items} />
+        <form onSubmit={this.handleSubmit}>
+          <input onChange={this.onChange} value={this.state.text} />
+          <button>{'Add #' + (this.state.items.length + 1)}</button>
+        </form>
+      </div>
+    );
   }
 });
 
-React.render(<Box />, document.getElementById('myApp'));
+React.render(<TodoApp />, document.getElementById('TodoApp'));
